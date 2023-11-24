f142c12a8550f53282d91193402a855bc67e9507
Martint1n <149445595+Martint1n@users.noreply.github.com>
2023-11-24 02:10:23 +0100

    adding remove button to books without prototype.

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 // book contient titre auteur pages (et "have you read it? checkbox")
-const newBook = {
+const startBook = {
     title: "Percy Jackson: The Lighting Thief",
     author: "Rick Riordan",
-    page: 472
+    pages: 472
 };
 
-const myLibrary = [newBook];
+const myLibrary = [startBook];
 const dialogBook = document.getElementsByClassName("dialogBook")
 const addBook = document.getElementsByClassName("btn");
 const storage = document.getElementsByClassName("storage");
-addBook[0].addEventListener("click", () =>{
+
+addBook[0].addEventListener("click", () => {
     //dialogBook[0].showModal();
     const newBook = new book;
     myLibrary.push(newBook);
+    addBookToLibrary()
     console.log(myLibrary[0]);
-    for (let livre = 0; livre < myLibrary.length; livre++){
-        const library = document.createElement("div");
-        library.style.border = "1px solid #F00"
-        library.innerHTML = myLibrary[livre];
-        console.log(myLibrary[livre]);
-        storage[0].append(library);
-    }
-
     console.log(newBook);
     console.log(myLibrary);
     console.log(storage[0].childNodes);
@@ -32,6 +26,33 @@ function book(title, author, pages) {
     this.author = prompt(author);
     this.pages = prompt(pages);
 }
-function addBookToLibrary() {
 
+/*function book(title, author, pages) {
+    this.title = "#title";
+    this.author = "#author"
+    this.pages = "pages"
+}*/
+
+function addBookToLibrary() {
+    //for (let livre = 0; livre < myLibrary.length; livre++){
+    const library = document.createElement("div");
+    const delet = document.createElement("button");
+    library.style.border = "1px solid #F00"
+    library.innerHTML = myLibrary[myLibrary.length-1].title + "<br />"+ myLibrary[myLibrary.length-1].author + "<br />" +myLibrary[myLibrary.length-1].pages;
+    //console.log(myLibrary[livre]);
+    //ajouter bouton remove au div
+    delet.textContent = "remove";
+    library.append(delet);
+    storage[0].append(library);
+    delet.addEventListener("click", () => {
+        library.remove();
+        
+    })
+    //}
+    console.log(Object.getPrototypeOf(myLibrary) === book.prototype); //-> reeturn true
+    //addlistener quand click invoque .remove
+    book.prototype.retirer = function() {
+        //library.remove(); works
+        
+    }
 }
